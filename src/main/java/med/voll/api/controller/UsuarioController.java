8a816601e21fd017d304c0694aae0c9238cb00ca
PxS00 <lucas.rossonidd@gmail.com>
2026-02-12 15:36:04 -0300

    feat: implement JWT token generation and update login response in UsuarioController

--- src/main/java/med/voll/api/controller/UsuarioController.java
+++ src/main/java/med/voll/api/controller/UsuarioController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.infrastructure.security.DadosTokenJWR;
+import med.voll.api.infrastructure.security.TokenService;
 import med.voll.api.model.usuario.DadosAutenticacao;
+import med.voll.api.model.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +21,15 @@ public class UsuarioController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+        var token = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new DadosTokenJWR(token));
     }
 }
---
+++ src/main/java/med/voll/api/infrastructure/security/DadosTokenJWR.java
@@ -0,0 +1,4 @@
+package med.voll.api.infrastructure.security;
+
+public record DadosTokenJWR(String token) {
+}
